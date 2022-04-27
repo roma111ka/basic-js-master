3b99c936dff91aae57a144aaab37134b1f0b1b95
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2022-04-27 22:53:50 +0300

    feat: fix names

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -15,10 +15,10 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getSumOfDigits( n ) {
   let strNumbers = n.toString();
   let sumDigits = 0;
-  for (let i = 0; i < str.length; i++) {
-    sum += Number.str[i];
+  for (let i = 0; i < strNumbers.length; i++) {
+    sumDigits += Number.strNumbers[i];
   }
-  return sum.toString().length > 1 ? sum = getSumOfDigits(sum) : sum;
+  return sumDigits.toString().length > 1 ? sumDigits = getSumOfDigits(sumDigits) : sumDigits;
 }
 
 module.exports = {
