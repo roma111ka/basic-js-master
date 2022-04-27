39c62891474012ed79c295d302fd464996874902
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2022-04-27 22:20:32 +0300

    feat: add fream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam( members ) {
+  if (!Array.isArray(members)) return false;
+  let nameDreamTeam = [];
+  for (let i = 0; i < members.length; i++) {
+    if(typeof members[i] === 'string') {
+      nameDreamTeam.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  nameDreamTeam.sort( (a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+    }).join('');
+    return nameDreamTeam;
 }
 
 module.exports = {
